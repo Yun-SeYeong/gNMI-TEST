66e99db3cd960c15a9c6862bc821ea9bfd7a3504
Yun-SeYeong <dbstpdud09@naver.com>
2021-12-13 11:38:30 +0900

    [UPDATE] update client

--- src/main/java/com/demo/main.java
+++ src/main/java/com/demo/main.java
@@ -13,44 +13,49 @@ import io.grpc.stub.MetadataUtils;
 
 public class main {
     public static void main(String[] args) {
+        String host = "192.168.100.162:5901";
+        String username = "admin";
+        String password = "admin";
+
+        if (args.length > 0 && !args[0].equals("")){
+            host = args[0];
+        }
+
+        if (args.length > 1 && !args[1].equals("")){
+            username = args[1];
+        }
+
+        if (args.length > 2 && !args[2].equals("")){
+            password = args[2];
+        }
+
         GetRequest getRequest = GetRequest.newBuilder()
-            .setPrefix(Path
-                .newBuilder()
-                .setTarget("192.168.100.172:5901"))
-            .addPath(Path.newBuilder()
-                .addElement("interfaces")
-                .addElement("interface[name=Ethernet2]")
-                .addElement("state")
-                .setOrigin("openconfig")
-                .addElem(
-                    PathElem.newBuilder()
-                        .setName("interfaces")
-                        .build()
-                )
-                .addElem(
-                    PathElem.newBuilder()
-                        .setName("interface")
-                        .putKey("name", "Ethernet2")
-                        .build()
-                )
-                .addElem(
-                    PathElem.newBuilder()
-                        .setName("state")
-                        .build()
-                )
-                .build())
-            .build();
+                .addPath(Path.newBuilder()
+                        .setOrigin("openconfig")
+                        .addElem(
+                                PathElem.newBuilder()
+                                        .setName("interfaces")
+                                        .build()
+                        )
+                        .addElem(
+                                PathElem.newBuilder()
+                                        .setName("interface")
+                                        .putKey("name", "Ethernet2")
+                                        .build()
+                        )
+                        .build())
+                .build();
 
       System.out.println("getRequest = " + getRequest);
 
       Channel channel = NettyChannelBuilder
-          .forTarget("192.168.100.172:5901")
+          .forTarget(host)
           .usePlaintext()
           .build();
 
       Metadata headers = new Metadata();
-      headers.put(Metadata.Key.of("username", Metadata.ASCII_STRING_MARSHALLER), "admin");
-      headers.put(Metadata.Key.of("password", Metadata.ASCII_STRING_MARSHALLER), "admin");
+      headers.put(Metadata.Key.of("username", Metadata.ASCII_STRING_MARSHALLER), username);
+      headers.put(Metadata.Key.of("password", Metadata.ASCII_STRING_MARSHALLER), password);
 
       gNMIBlockingStub gNMIStub = MetadataUtils.attachHeaders(gNMIGrpc.newBlockingStub(channel), headers);
 
